5904e9cdc43aa37e50b099c6e50c79ad5f071746
kyleerod <kyleerod@student.21-school.ru>
2022-11-13 17:52:20 +0700

    gb

--- GridBuilder/Form1.Designer.cs
+++ GridBuilder/Form1.Designer.cs
@@ -31,16 +31,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(542, 373);
+            this.button1.Location = new System.Drawing.Point(580, 257);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Set a field";
+            this.button1.Text = "Set a field...";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -51,22 +54,58 @@
             this.pictureBox1.Size = new System.Drawing.Size(480, 380);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(542, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set an additional  grid...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(542, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Make grid";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Grid Builder";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,6 +114,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GridBuilder/Form1.cs
+++ GridBuilder/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* Lab 1
+ * Var 21
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,21 +21,26 @@ namespace GridBuilder
     public partial class Form1 : Form
     {
         public List<Point> fieldPoints;
+        public List<Point> node;
+        public List<int> addx;
+        public List<int> addy;
+        public List<float> gridx;
+        public List<float> gridy;
+        public Graphics g;
+        Bitmap bmp = new Bitmap(480, 380);
+
+        public int xval, yval;
+        public int xcrsh = 9; //X crushing
+        public int ycrsh = 9; //Y crushing
+
         public Form1()
         {
             fieldPoints = new List<Point>();
-            /*fieldPoints.Add(new Point(30, 100));
-            fieldPoints.Add(new Point(3,160));
-            fieldPoints.Add(new Point(3, 300));
-            fieldPoints.Add(new Point(340, 300));
-            fieldPoints.Add(new Point(340,120));
-            fieldPoints.Add(new Point(240, 150));
-            fieldPoints.Add(new Point(240, 180));
-            fieldPoints.Add(new Point(100, 180));
-            fieldPoints.Add(new Point(100, 100));
-            fieldPoints.Add(new Point(30, 100));*/
-
+            addx = new List<int>();
+            addy = new List<int>();
+            node = new List<Point>();
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -43,23 +51,26 @@ namespace GridBuilder
         // Drawning func
         private void drawField(List<Point> dot)
         {
-            Graphics g = pictureBox1.CreateGraphics();
+            
+            g = Graphics.FromImage(bmp);
             Pen fieldPen;
             fieldPen = new Pen(Brushes.Black, 4);
             g.Clear(Color.White);
             string[] datas = getField();
+            if (datas == null) return;
             for (int i = 1; i < int.Parse(datas[0]); i++)
             {
                 g.DrawLine(fieldPen, dot[i-1], dot[i]);
             }
-
+            pictureBox1.Image = bmp;
+            button2.Enabled = true;
         }
 
         //Reading func
         private string[] getField()
         {
             string[] datas;
-            string[] empty = { };
+            string[] empty = null;
             if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
                 return empty;
             string filename = openFileDialog1.FileName;
@@ -73,7 +84,150 @@ namespace GridBuilder
             }
             return datas;
         }
-        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pen addGridPen;
+            addGridPen = new Pen(Brushes.Gray, 1);
+            addGridPen.DashPattern = new float[] { 4.0F, 2.0F, 1.0F, 3.0F };
+            getAddGrid();
+            for(int i = 0; i < yval; i++)
+            {
+                for (int j = 0; j < xval; j++)
+                {
+                    Point y0 = new Point(addx[0], addy[j]);
+                    Point x0 = new Point(addx[i], addy[0]);
+                    Point cross = new Point(addx[i], addy[j]);
+                    g.DrawLine(addGridPen, y0, cross);
+                    g.DrawLine(addGridPen, cross, x0);
+                } 
+            }
+            pictureBox1.Image = bmp;
+            button3.Enabled = true;
+        }
+
+        private void getAddGrid()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            string[] vals = System.IO.File.ReadAllLines(filename);
+            xval = int.Parse(vals[0]);
+            yval = int.Parse(vals[2]);
+            string[] x = vals[1].Split('\t');
+            for (int i = 0; i < xval; i++)
+            {
+                addx.Add(int.Parse(x[i]));
+            }
+            string[] y = vals[3].Split('\t');
+            for (int i = 0; i < xval; i++)
+            {
+                addy.Add(int.Parse(y[i]));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Vertical
+            Pen gridPen = new Pen(Brushes.Red, 1);
+                for(int j = 0; j<=xcrsh; j++)
+                {
+                //int xt = addx[i-1] + (addx[i] - addx[i-1]) / xcrsh * j;
+                double xtd = 1.5 + addx[0] + (addx[xval - 1] - addx[0] - 4.5 ) / xcrsh * j;
+                int xt = Convert.ToInt32(Math.Round(xtd));
+                Point y0 = new Point(xt, addy[0]);
+                    int yt = findy(xt);
+                    Point ym = new Point(xt, yt);
+                    g.DrawLine(gridPen, y0, ym);
+                    node.Add(y0);
+                    node.Add(ym);
+                }
+
+            // Horizontal
+
+                for (int j = 0; j <= ycrsh; j++)
+                {
+               // int yt = addy[yval-1]+(addy[0] - addy[yval - 1]) * j / ycrsh;
+                double ytd = 1.5 + addy[yval-1]+(addy[0] - addy[yval - 1] -4.5) * j / ycrsh;
+                int yt = Convert.ToInt32(Math.Round(ytd));
+                    drawHor(yt, gridPen);
+                }
+
+            pictureBox1.Image = bmp;
+            saveNode();
+        }
+
+        private void drawHor(int y, Pen pen)
+        {
+            // Colors for the search 
+            Color c = Color.FromArgb(255, 0, 0);
+            Color border = Color.FromArgb(0, 0, 0);
+
+            List<int> xp = new List<int>();
+            // Width of the paintBox is 480
+
+            xp.Add(0);
+            while( c != border)
+            {
+                c = bmp.GetPixel(xp[0], y);
+                xp[0]++;
+            }
+            
+            for( int i = 1; i < xval; i++)
+            {
+                int xwd = addx[i] - 3;
+                int id = 0;
+                for (int h = 1; h < 7; h++)
+                {
+                    c = bmp.GetPixel(xwd+h, y);
+                    if (c == border) id = 1;
+                }
+                if (id == 1) xp.Add(addx[i]);
+            }
+
+            for (int m = 1; m < xp.Count; m += 2)
+            {
+                Point p2 = new Point(xp[m], y);
+                Point p1 = new Point(xp[m - 1], y);
+                g.DrawLine(pen, p1, p2);
+                node.Add(p1);
+                node.Add(p2);
+            }
+        }
+
+        private int findy(int x)
+        {
+            Color c = Color.FromArgb(255, 0, 0);
+            Color admesh = Color.FromArgb(128, 128, 128);
+            Color border = Color.FromArgb(0, 0, 0);
+            int res = addy[0] - 5;
+            while (c != border) 
+            {
+                c = bmp.GetPixel(x, res);
+                res--;
+            }
+            return res;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string coord = "";
+            coord+= e.X.ToString();
+            coord+= ", ";
+            coord+= e.Y.ToString();
+            label1.Text = coord;
+        }
+        // Makes nodes file
+        private void saveNode()
+        {
+            string nodes = "X\tY\n";
+            string filename = "..\\...\\...\\node.txt";
+            for(int i = 0; i < node.Count; i++)
+            {
+                nodes += node[i].X.ToString() + '\t' + node[i].Y.ToString() + '\n';
+            }
+            System.IO.File.WriteAllText(filename, nodes);
+        }
     }
  
 
